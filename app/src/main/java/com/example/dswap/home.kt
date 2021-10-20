c58797daed5f55113bec303b65c1fc6a3137f374
McKael-forti <76393985+Mckael123@users.noreply.github.com>
2021-10-20 21:43:43 +0700

    update fragmen home and template fragment for symptomps and help fragmen

--- app/src/main/java/com/example/dswap/home.kt
+++ app/src/main/java/com/example/dswap/home.kt
@@ -2,10 +2,39 @@ package com.example.dswap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val home     = homeFragment()
+        val symtomps = SymptomsFragment()
+        val help     = HelpFragment()
+        val navbtn = findViewById(R.id.bottomNavigationView) as BottomNavigationView
+        CurrentFragmentSet(home)
+
+        navbtn.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> CurrentFragmentSet(home)
+                R.id.symptoms -> CurrentFragmentSet(symtomps)
+                R.id.help -> CurrentFragmentSet(help)
+            }
+            true
+        }
+
+
+    }
+
+    private fun CurrentFragmentSet(fragment:Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/example/dswap/homeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.dswap
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [homeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class homeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
