51b0f63e62fc73aa7e8dfa84eba533204aef9b50
ArlPrima <81967164+ArlPrima@users.noreply.github.com>
2021-11-01 23:46:51 +0700

    Update Tugas 1
    Bagian Tugas 1 punyaku dah kelar semua ges

--- app/src/main/java/com/example/dswap/reservation.kt
+++ app/src/main/java/com/example/dswap/reservation.kt
@@ -2,10 +2,33 @@ package com.example.dswap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class reservation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation)
+        val registration = RegistFragment()
+        val detail = DetailFragment()
+        val pay  = PayFragment()
+        val navbtnRegist   = findViewById(R.id.bottomNavigationViewRegist) as BottomNavigationView
+        CurrentFragmentSet(registration)
+        navbtnRegist.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.registration -> CurrentFragmentSet(registration)
+                R.id.detail -> CurrentFragmentSet(detail)
+                R.id.pay -> CurrentFragmentSet(pay)
+            }
+            true
+        }
+    }
+
+    private fun CurrentFragmentSet(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment2, fragment)
+            commit()
+
+        }
     }
 }
