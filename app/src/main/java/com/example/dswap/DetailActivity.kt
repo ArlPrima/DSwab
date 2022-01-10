0a6a4d823f8da451b5e36cc9ee77c5dfe6f3b9b8
McKael-forti <76393985+Mckael123@users.noreply.github.com>
2022-01-10 19:10:40 +0700

    update date picker form

--- app/src/main/java/com/example/dswap/DetailActivity.kt
+++ app/src/main/java/com/example/dswap/DetailActivity.kt
@@ -1,6 +1,9 @@
 package com.example.dswap
 
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.*
@@ -10,15 +13,18 @@ import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.database.FirebaseDatabase
 import com.kofigyan.stateprogressbar.StateProgressBar
+import java.util.*
 
 
 class DetailActivity : AppCompatActivity() {
     protected var descriptionData = arrayOf("Registration", "Detail", "Payment")
 
+
     companion object{
         const val EXTRA_name = "extra_name"
 
     }
+    var onDateSetListener: DatePickerDialog.OnDateSetListener? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -27,9 +33,10 @@ class DetailActivity : AppCompatActivity() {
         var namePatient = findViewById<EditText>(R.id.patientName)
         var textNIK = findViewById<EditText>(R.id.Nik)
         var textTypeTest = findViewById<AutoCompleteTextView>(R.id.jenisTesBtn)
-        var datefield = findViewById<EditText>(R.id.tanggalTes)
+        var datefield = findViewById<AutoCompleteTextView>(R.id.DateFormat)
         var AddressText = findViewById<EditText>(R.id.alamatTes)
         var textKlinik = findViewById<AutoCompleteTextView>(R.id.klinikInput)
+
 
         //pass data
         val Email = intent.getStringExtra(EXTRA_name)
@@ -55,12 +62,31 @@ class DetailActivity : AppCompatActivity() {
 //        (textTypeTest.editText as? AutoCompleteTextView)?.setAdapter(tesAdapter)
         textTypeTest.setAdapter(tesAdapter)
 
-        datefield.setOnClickListener() {
-            MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Select date")
-                .build()
-        }
+        //date input operation
 
+        val calendar = Calendar.getInstance()
+        val year = calendar[Calendar.YEAR]
+        val month = calendar[Calendar.MONTH]
+        val day = calendar[Calendar.DAY_OF_MONTH]
+        datefield.setOnClickListener(View.OnClickListener {
+            val datePickerDialog = DatePickerDialog(
+                this, android.R.style.
+                    Theme_Holo_Light_Dialog_MinWidth,
+
+                onDateSetListener, year, month, day
+            )
+            datePickerDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            datePickerDialog.show()
+        })
+        onDateSetListener =
+            DatePickerDialog.OnDateSetListener { datePicker, year, month, dayofMonth ->
+                var month = month
+                month = month + 1
+                val date = "$dayofMonth/$month/$year"
+                datefield.setText(date)
+            }
+
+        //intent logo
         logoDetail.setOnClickListener {
             var ngintenDetail = Intent(this, home::class.java)
             startActivity(ngintenDetail)
